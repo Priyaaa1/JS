9a5d99160523b9cbac03567d9f6f8f2350c929fd
Priya <105431748+Priyaaa1@users.noreply.github.com>
2025-06-13 21:38:52 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "priya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gwalior"
+let accountState;
+
+// accountId = 2   // not allowed
+// console.log(accountId);
+
+
+accountEmail = "pm@gmail.com"
+accountPassword = "123456"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+
+/*
+Primitive datatypes:
+string
+number
+boolean
+bigint
+null
+undefined
+symbol
+
+object
+*/
+
+console.log(typeof "wow");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 2);
