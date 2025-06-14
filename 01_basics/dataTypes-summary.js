93db6ca42a95945c95ed376d64105f3e2ab32ac6
Priya <105431748+Priyaaa1@users.noreply.github.com>
2025-06-14 17:51:09 +0000

    datatypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, symbol, BigInt
+
+
+// Reference type ( Non-Primitive )
+
+// Array, Objects, Functions
+
+const heros = ["spiderman", "ironman", "shaktiman"];
+let  myObj = {
+    name: "priya",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+} 
+
+// console.log(typeof myFunction); // function object
+// console.log(typeof(myObj.name)); // string
+
