136c437880a46e019d6ea42c407e92bea9cf9fe9
Priya <105431748+Priyaaa1@users.noreply.github.com>
2025-06-14 22:32:49 +0000

    strings

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,26 @@
+const name = "priya"
+const repoCount = 40
+
+// console.log(name + repoCount + "value"); // not recommended
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('priyapm')
+
+// console.log(gameName[0]); // p
+// console.log(gameName.__proto__); // {}
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2)); // i
+// console.log(gameName.indexOf('a')); // 4
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const url = "https://priya.com/priya%20pm"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('pm'))
+
+const name1 = "my-name-is-priya"
+// console.log(name1.split('-')); // [ 'my', 'name', 'is', 'priya' ]
+
