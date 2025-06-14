3a6294242524e6115d7f1a9afedf051b40f52496
Priya <105431748+Priyaaa1@users.noreply.github.com>
2025-06-14 07:31:55 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = "33ab"
+
+// console.log(typeof score);  //string
+// console.log(typeof(score));  //string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); //NaN
+
+/*
+"33" => 33
+"33ab" => NaN
+true => 1, false => 0
+null => 0
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
