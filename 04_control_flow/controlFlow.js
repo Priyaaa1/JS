5da091bd441a5d206db55baafc1b4e0d012a55ae
Priya <105431748+Priyaaa1@users.noreply.github.com>
2025-06-18 21:29:42 +0000

    control flow

---
+++ 04_control_flow/controlFlow.js
@@ -0,0 +1,22 @@
+// if, else if, else
+
+// switch case
+
+// truthy - true, "0", 'false', " ", [], {}, function(){}
+
+// falsy - false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+
+// Nullish Coalescing Operator (??) : null, undefined
+let val1;
+// val1 = 5 ?? 10
+// console.log(val1); // 5
+
+// val1 = null ?? 10
+// console.log(val1); // 10
+
+// val1 = undefined ?? 15
+// console.log(val1); // 15
+
+
+// Terniary Operator
